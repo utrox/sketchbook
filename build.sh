35fc2069a3ffedcc250e2e4b5fdd17d1c74a4cc2
Bence Hunyadi <hunyadi.bence1@gmail.com>
2024-12-03 17:55:16 +0200

    Update render config

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r ./backend/requirements.txt
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+python ./backend/manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py migrate
+python ./backend/manage.py migrate
