0d247edc315c6e979f9b05a91beaedb8c914da3b
Bence Hunyadi <hunyadi.bence1@gmail.com>
2024-12-03 21:40:39 +0200

    Update render config

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -173,9 +173,9 @@ mimetypes.add_type("text/javascript", ".js", True)
 
 # Render.com: Django settings for static assets
 # This production code might break development mode, so we check whether we're in DEBUG mode
-if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    import os
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+# and renames the files with unique names for each version to support long-term caching
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
