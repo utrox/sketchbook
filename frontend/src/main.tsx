7b625999f107770e2eaa152533519dc91a3261ec
Bence Hunyadi <hunyadi.bence1@gmail.com>
2025-01-18 13:00:24 +0200

    Merge branch 'main' into 90__dry_auth_requests

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,9 +8,8 @@ import {
   ApolloProvider,
   InMemoryCache,
 } from "@apollo/client";
-import createUploadLink from "apollo-upload-client/createUploadLink.mjs";
-
 import { onError } from "@apollo/client/link/error";
+import createUploadLink from "apollo-upload-client/createUploadLink.mjs";
 
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -20,20 +19,6 @@ import "bootstrap/dist/css/bootstrap.css";
 
 import App from "./App.tsx";
 import { getCSRFToken } from "./utils";
-
-
-// Read the CSRF Token from the cookie.
-const getCSRFToken = (): string => {
-  const name = "csrftoken";
-  const cookies = document.cookie.split("; ");
-
-  for (const cookie of cookies) {
-    const [key, value] = cookie.split("=");
-    if (key === name) return value;
-  }
-
-  return "";
-};
 
 // Error-handler, when the grapQL API returns an error,
 // we're going to display the user-friendly message to the user
